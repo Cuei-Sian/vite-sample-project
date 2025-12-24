1bdcb1ae03bb567a27df4094ec241d160d7ba71c
doreen <doreen20080615@gmail.com>
2025-12-25 00:15:28 +0800

    部署gh-pages和載入axios

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
+// export default defineConfig({
+//   // 拆分開發中和完成產品的路徑
+//   base: process.env.NODE_ENV === "production" ? "/vite-sample-project/" : "/",
+//   plugins: [react()],
+// });
+
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/vite-sample-project/" : "/",
   plugins: [react()],
-})
+}));
